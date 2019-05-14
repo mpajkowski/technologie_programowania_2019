e99cc10008967cab526d8ae4f67dcc0f9f7c9e87
Marcin Pajkowski <211968@edu.p.lodz.pl>
2019-05-14 21:58:20 +0200

    Create a casino dll
    Move to new directory

--- zad_1/casino/Properties/AssemblyInfo.cs
+++ zad_1/casino/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("kasyno")]
+[assembly: AssemblyTitle("casino")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("kasyno")]
+[assembly: AssemblyProduct("casino")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

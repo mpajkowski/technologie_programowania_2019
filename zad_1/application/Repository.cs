834b0a0722ae5227509e01b40cb33c475924de2a
Marcin Pajkowski <211968@edu.p.lodz.pl>
2019-05-14 23:00:08 +0200

    Add DataContext and Repository classes

---
+++ zad_1/application/Repository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace application
+{
+    class Repository
+    {
+        private DataContext dataContext;
+
+        Repository()
+        {
+            dataContext = new DataContext();
+        }
+    }
+}
